d1f797318755b0bbddf330277d57b81a945f483d
wadud115 <wadud871226@gmail.com>
2026-04-01 19:25:35 +0600

    navber and banner design complte

--- src/component/Banner.jsx
+++ src/component/Banner.jsx
@@ -41,7 +41,7 @@ Explore Products
         <div className="relative flex justify-center lg:justify-end">
           <div className="relative">
             <img
-              className="relative h-130 w-auto object-contain drop-shadow-2xl rounded-3xl "
+              className="relative h-130 w-auto object-contain drop-shadow-xl rounded-3xl "
               src={BannerImage}
               alt=""
             />
